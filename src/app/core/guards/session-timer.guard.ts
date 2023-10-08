c14e7df7aea00aa53d69287c9c34998be074fd63
André Levi <andrelevi767@gmail.com>
2023-10-08 19:13:08 -0300

    timer guard added, needing fix

--- src/app/core/guards/session-timer.guard.ts
+++ src/app/core/guards/session-timer.guard.ts
@@ -16,18 +16,14 @@ export class SessionTimerGuard implements CanActivate {
       const currentTime = Date.now();
 
       if (currentTime - lastActivityTime < timeoutDuration) {
-        // O tempo desde a última atividade é menor que o limite de tempo limite
-        // Atualize o tempo da última atividade para o momento atual
         this._authService.updateLastActivityTime();
         return true;
       } else {
-        // O tempo desde a última atividade ultrapassou o limite de tempo limite
         this._authService.logout();
         this._router.navigate(['/']);
         return false;
       }
     } else {
-      // Usuário não autenticado, redireciona para a página de login
       this._router.navigate(['/login']);
       return false;
     }
